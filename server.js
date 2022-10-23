c2fda5d816009c86a0bfa998c8cdb5225a3ac88a
alasidig <ala.sidig@gmail.com>
2022-10-23 15:02:32 +0300

    Serve static files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+app.use(express.static('public'))
 app.get('/', (req, res) => res.send('<h2>Welcome to the Flowers Shop Express server</h2>'))
 app.listen(port, () => console.log(`Example app listening on port http://127.0.0.1:${port}`))
